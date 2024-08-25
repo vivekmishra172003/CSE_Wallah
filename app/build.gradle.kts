d16f472cee66cd962f1f3ee46838b1354355a609
Vivek Mishra <vivekmishra172003@gmail.com>
2024-08-25 17:48:19 +0530

    implemented view bindings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // added by me for splash screen
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
